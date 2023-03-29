71aa95635e08a190ef8e25d3d053c4953f8dfdaa
mohamed154salah <mohamed156salah@gmail.com>
2023-03-29 16:15:56 +0200

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+    before_action :require_login, only:[:new,:create]
+    def home
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        if @post.save
+            redirect_to posts_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def require_login
+        unless user_signed_in?
+            flash[:alert] = "You must be logged in to access this section"
+            redirect_to new_user_session_path # halts request cycle
+        end
+
+    end
+
+    def post_params
+        params.require(:post).permit(:title,:body)
+    end
+
+end
